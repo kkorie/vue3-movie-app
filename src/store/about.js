af57e6b1692329915ca19461df2d82754ab24869
kkorie <devkorie@gmail.com>
2021-08-19 21:48:06 +0900

    Create project

---
+++ src/store/about.js
@@ -0,0 +1,9 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'KKORIE',
+    email: 'devkorie@gmail.com',
+    github: 'https://github.com/kkorie',
+    image: 'https://heropy.blog/css/images/logo.png'
+  })
+}
